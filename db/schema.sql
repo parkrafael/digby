3785555d88cf0d4fc25acb278a4f0f0ec69b8f04
Rafael Park <parkrafael02@gmail.com>
2026-03-18 12:59:57 -0700

    feat: database schema

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE TABLE users (
+    email           TEXT PRIMARY KEY,
+    agent_id        TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE images (
+    image_id        TEXT PRIMARY KEY,
+    agent_id        TEXT REFERENCES users(agent_id),
+    file_name       TEXT NOT NULL,
+    file_type       TEXT NOT NULL,
+    file_timestamp  TIMESTAMPTZ NOT NULL,
+    embedding       vector(512),
+    tags            TEXT[],
+    created_at      TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE albums (
+    album_id        TEXT PRIMARY KEY,
+    album_name      TEXT NOT NULL,
+    agent_id        TEXT REFERENCES users(agent_id),
+    created_at      TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE albums_images(
+    album_id        TEXT REFERENCES albums(album_id),
+    image_id        TEXT REFERENCES images(image_id),
+    PRIMARY KEY (album_id, image_id)
+);
+
+CREATE TABLE share_tokens (
+    token           TEXT PRIMARY KEY,
+    album_id        TEXT REFERENCES albums(album_id),
+    expires_at      TIMESTAMPTZ NOT NULL,
+    created_at      TIMESTAMPTZ DEFAULT now()
+);
